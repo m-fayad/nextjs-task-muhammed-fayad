6939e666d90258ab94319f7375e294b175637d88
Nyctophilus <mohammed.yuossry@gmail.com>
2025-10-20 21:58:16 +0300

    init project setup

---
+++ components/layout/aside-section.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AsideSection = () => {
+  return <aside className="space-y-8">aside</aside>;
+};
+
+export default AsideSection;
---
+++ components/layout/main-section.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainSection = () => {
+  return <main className="space-y-8">Main</main>;
+};
+
+export default MainSection;
